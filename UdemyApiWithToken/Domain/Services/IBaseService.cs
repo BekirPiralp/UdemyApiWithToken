46afbc9c0fd0c93af4a3a677027b68ac072fc463
Bekir PİRALP <bpir33@gmail.com>
2022-06-10 15:12:14 +0300

    Service interface oluşturuldu

---
+++ UdemyApiWithToken/Domain/Services/IBaseService.cs
@@ -0,0 +1,15 @@
+﻿using UdemyApiWithToken.Domain.Response;
+
+namespace UdemyApiWithToken.Domain.Services
+{
+    public interface IBaseService<TEntity,IRespose>
+        where TEntity : class
+        where IRespose : IBaseResponse<TEntity>,new()
+    {
+        Task<IRespose> ListAsync();
+        Task<IRespose> AddAsync(TEntity entity);
+        Task<IRespose> RemoveAsync(int entityId);
+        Task<IRespose> UpdateAsync(TEntity entity,int entityId);
+        Task<IRespose> FindByIdAsync(int entityId);
+    }
+}
---
+++ UdemyApiWithToken/Domain/Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using UdemyApiWithToken.Domain.Model;
+using UdemyApiWithToken.Domain.Response;
+
+namespace UdemyApiWithToken.Domain.Services
+{
+    public interface IProductService:IBaseService<Product,ProductResponse>
+    {
+    }
+}
