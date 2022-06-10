9ef599773e188e164b846c10f5d546a3cb812c49
Bekir PİRALP <bpir33@gmail.com>
2022-06-10 12:04:01 +0300

    gerekli yerler kaldırıldı

--- UdemyApiWithToken/Domain/Entities/UdemyApiWithTokenContext.cs
+++ UdemyApiWithToken/Domain/Entities/UdemyApiWithTokenContext.cs
@@ -20,47 +20,47 @@ namespace UdemyApiWithToken.Domain.Entities
         public virtual DbSet<Product> Products { get; set; } = null!;
         public virtual DbSet<User> Users { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Data Source=DESKTOP-IBLI6B4;Initial Catalog=UdemyApiWithToken;Integrated Security=True");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+////#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+////                optionsBuilder.UseSqlServer("Data Source=DESKTOP-IBLI6B4;Initial Catalog=UdemyApiWithToken;Integrated Security=True");
+//            }
+//        }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Product>(entity =>
-            {
-                entity.ToTable("Product");
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            modelBuilder.Entity<Product>(entity =>
+//            {
+//                entity.ToTable("Product");
 
-                entity.Property(e => e.Category).HasMaxLength(50);
+//                entity.Property(e => e.Category).HasMaxLength(50);
 
-                entity.Property(e => e.Name).HasMaxLength(50);
+//                entity.Property(e => e.Name).HasMaxLength(50);
 
-                entity.Property(e => e.Price).HasColumnType("money");
-            });
+//                entity.Property(e => e.Price).HasColumnType("money");
+//            });
 
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.ToTable("User");
+//            modelBuilder.Entity<User>(entity =>
+//            {
+//                entity.ToTable("User");
 
-                entity.Property(e => e.Email).HasMaxLength(100);
+//                entity.Property(e => e.Email).HasMaxLength(100);
 
-                entity.Property(e => e.Name).HasMaxLength(50);
+//                entity.Property(e => e.Name).HasMaxLength(50);
 
-                entity.Property(e => e.Password).HasMaxLength(8);
+//                entity.Property(e => e.Password).HasMaxLength(8);
 
-                entity.Property(e => e.RefreshToken).HasMaxLength(500);
+//                entity.Property(e => e.RefreshToken).HasMaxLength(500);
 
-                entity.Property(e => e.RefreshTokenEndDate).HasColumnType("datetime");
+//                entity.Property(e => e.RefreshTokenEndDate).HasColumnType("datetime");
 
-                entity.Property(e => e.SurName).HasMaxLength(50);
-            });
+//                entity.Property(e => e.SurName).HasMaxLength(50);
+//            });
 
-            OnModelCreatingPartial(modelBuilder);
-        }
+//            OnModelCreatingPartial(modelBuilder);
+//        }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
