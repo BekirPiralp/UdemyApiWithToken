b7c50bae3fd7d5627a22987850e20f8221e4de4d
Bekir PİRALP <bpir33@gmail.com>
2022-06-11 21:32:26 +0300

    auto map ile convert yapıldı. hata mesajları getittirildi.

---
+++ UdemyApiWithToken/Domain/Extensions/ModelStateExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace UdemyApiWithToken.Domain.Extensions
+{
+    public static class ModelStateExtension
+    {
+        // hata mesajlarını getittiriyoruz
+        public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+        {
+            return dictionary.SelectMany(m=>m.Value.Errors).Select(x=>x.ErrorMessage).ToList();
+        }
+    }
+}
---
+++ UdemyApiWithToken/Mapping/ProductMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UdemyApiWithToken.Domain.Model;
+using UdemyApiWithToken.Resources;
+
+namespace UdemyApiWithToken.Mapping
+{
+    public class ProductMapping:Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<ProductResource, Product>();
+            CreateMap<Product,ProductResource>();
+        }
+    }
+}
