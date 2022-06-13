36f74d2b3f0862cd9d48994e1f6f98550e60a2b1
Bekir PİRALP <bpir33@gmail.com>
2022-06-13 16:22:29 +0300

    tokenhandler kurucu methodu ayarlandı

--- UdemyApiWithToken/Security/Token/TokenHandler.cs
+++ UdemyApiWithToken/Security/Token/TokenHandler.cs
@@ -1,9 +1,16 @@
-﻿using UdemyApiWithToken.Domain.Model;
+﻿using Microsoft.Extensions.Options;
+using UdemyApiWithToken.Domain.Model;
 
 namespace UdemyApiWithToken.Security.Token
 {
     public class TokenHandler : ITokenHandler
     {
+        TokenOptions _tokenOptions;
+        public TokenHandler(IOptions<TokenOptions> tokenOptions)
+        {
+            _tokenOptions = tokenOptions.Value;
+        }
+
         public AccessToken CreateAccessToken(User user)
         {
             throw new NotImplementedException();
