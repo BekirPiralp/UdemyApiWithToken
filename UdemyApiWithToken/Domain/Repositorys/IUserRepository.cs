a8cb7dad244b842dee6dc6737ff1617767cc2b25
Bekir PİRALP <bpir33@gmail.com>
2022-06-15 09:51:58 +0300

    IUSer Repository yazıldı

---
+++ UdemyApiWithToken/Domain/Repositorys/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using UdemyApiWithToken.Domain.Model;
+
+namespace UdemyApiWithToken.Domain.Repositorys
+{
+    public interface IUserRepository
+    {
+        Task AddUSer(User user);
+
+        User FindById(int id);
+
+        User FindByEmailAndPassword(string email, string password);
+
+        void SaveRefreshToken(int userId, string refreshToken);
+
+        User GetUserWithRefreshToken(string refreshToken);
+
+        void RemoveRefreshToken(User user);
+    }
+}
