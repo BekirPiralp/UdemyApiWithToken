80df2e3ce610b60453d6f606cf715d4719771c7f
Bekir PİRALP <bpir33@gmail.com>
2022-06-15 10:33:38 +0300

    user response oluşturuldu

---
+++ UdemyApiWithToken/Domain/Response/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using UdemyApiWithToken.Domain.Model;
+
+namespace UdemyApiWithToken.Domain.Response
+{
+    public class UserResponse : BaseResponse<User>
+    {
+        public UserResponse(string message) : base(message)
+        {
+        }
+
+        public UserResponse(User user) : base(user)
+        {
+        }
+    }
+}
