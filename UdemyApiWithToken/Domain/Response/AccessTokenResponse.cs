796e7b24de1e769e1444ef48fcfcf2db01d2551a
Bekir PİRALP <bpir33@gmail.com>
2022-06-15 13:32:04 +0300

    IAuthenticationservice yazıldı.

---
+++ UdemyApiWithToken/Domain/Response/AccessTokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using UdemyApiWithToken.Security.Token;
+
+namespace UdemyApiWithToken.Domain.Response
+{
+    public class AccessTokenResponse : BaseResponse<AccessToken>
+    {
+        public AccessTokenResponse(AccessToken entity) : base(entity)
+        {
+        }
+        public AccessTokenResponse(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ UdemyApiWithToken/Domain/Services/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using UdemyApiWithToken.Domain.Response;
+
+namespace UdemyApiWithToken.Domain.Services
+{
+    public interface IAuthenticationService
+    {
+        AccessTokenResponse CreateAccessToken(string email,string password);
+        AccessTokenResponse CreateAccessTokenByRefreshToken(string refreshToken);
+        AccessTokenResponse RemoveRefreshToken(string refreshToken);
+    }
+}
