d8729fd83f332598b2375fdfbfd69145bb7b4874
Bekir PİRALP <bpir33@gmail.com>
2022-06-15 11:04:26 +0300

    User Mapping yazıldı

---
+++ UdemyApiWithToken/Mapping/UserMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UdemyApiWithToken.Domain.Model;
+using UdemyApiWithToken.Resources;
+
+namespace UdemyApiWithToken.Mapping
+{
+    public class UserMapping:Profile
+    {
+        public UserMapping()
+        {
+            CreateMap<User, UserResource>();
+            CreateMap<UserResource, User>();
+        }
+    }
+}
