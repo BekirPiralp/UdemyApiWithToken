1f4e17da0cc3a79ead08a15ee6075644dd428e84
Bekir PİRALP <bpir33@gmail.com>
2022-06-16 09:35:28 +0300

    Swagera 'a authorization özeliği getirildi

---
+++ UdemyApiWithToken/Extensions/SwagerAddExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace UdemyApiWithToken.Extensions
+{
+    public static class SwagerAddExtension
+    {
+        public static void SwagerAddAuthorization(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(
+                    opt =>{
+                        opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Udemy eğitimi API", Version = "v01" });
+
+                        opt.AddSecurityDefinition("Bearer",new OpenApiSecurityScheme{
+                            In = ParameterLocation.Header,
+                            Description = "Lütfen geçerli token'ı giriniz.",
+                            Name = "İzin al",
+                            Type = SecuritySchemeType.Http,
+                            BearerFormat = "JWT",
+                            Scheme = "Bearer"
+                        });
+
+                        var oASS = new OpenApiSecurityScheme{
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        };
+
+                        var value = new List<String>(); 
+                        opt.AddSecurityRequirement(new OpenApiSecurityRequirement{{
+                                oASS,
+                                value
+                            }
+                        }) ;
+                    }
+                );
+        }
+    }
+}
--- UdemyApiWithToken/Program.cs
+++ UdemyApiWithToken/Program.cs
@@ -12,7 +12,9 @@ builder.Services.Addjwt(builder);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.SwagerAddAuthorization();
+//builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<UdemyApiWithTokenContext>(
     opt =>
